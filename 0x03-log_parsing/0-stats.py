d325796d723e6556725b3a5c1ced92a92cc3cf06
Rebecca <damilolarebecca8@gmail.com>
2022-07-11 13:15:50 +0100

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,46 @@
 #!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics'''
-
+"""script that reads stdin line by line"""
 
 import sys
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-counter = 0
 
-try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
+if __name__ == "__main__":
+    status_code = {"200": 0,
+                   "301": 0,
+                   "400": 0,
+                   "401": 0,
+                   "403": 0,
+                   "404": 0,
+                   "405": 0,
+                   "500": 0}
+    on_ten = 1
+    file_size = 0
 
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
+    def parse_line(line):
+        """parse a line"""
+        try:
+            parsed_line = line.split()
+            code = parsed_line[-2]
+            if code in status_code.keys():
+                status_code[code] += 1
+            return int(parsed_line[-1])
+        except Exception:
+            return 0
 
-except Exception as err:
-    pass
+    def print_stats():
+        """display the stats"""
+        print("File size: {}".format(file_size))
+        for key in sorted(status_code.keys()):
+            if status_code[key]:
+                print("{}: {}".format(key, status_code[key]))
 
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+    try:
+        for line in sys.stdin:
+            file_size += parse_line(line)
+            if on_ten % 10 == 0:
+                print_stats()
+            on_ten += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
